898a015fc73799dd996eb2a9c6573b8d831bc7ee
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-03-14 12:27:22 -0300

    feat: create gyms controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { create } from './create'
+import { search } from './gyms'
+import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
